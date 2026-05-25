13370f9d606e7c37ab42a108ba29c388fa1be9d8
HarrySytems <cabanaharry4@gmail.com>
2026-05-24 19:14:36 -0500

    verificacion google y monetag

--- public/sw.js
+++ public/sw.js
@@ -10,3 +10,11 @@ self.addEventListener('fetch', (event) => {
   // Simple pass-through fetch so we don't interfere with large video file downloads/streaming
   event.respondWith(fetch(event.request));
 });
+
+self.options = {
+    "domain": "3nbf4.com",
+    "zoneId": 11053374
+}
+self.lary = ""
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+
